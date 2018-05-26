6364e1f1adcf94d2a4ee2370dacad410f53d8a4c
bu4y <amnes1a@windowslive.com>
2018-05-26 22:04:52 +0700

    update

--- modules/downloads/client/controllers/list-downloads.client.controller.js
+++ modules/downloads/client/controllers/list-downloads.client.controller.js
@@ -5,16 +5,21 @@
     .module('downloads')
     .controller('DownloadsListController', DownloadsListController);
 
-  DownloadsListController.$inject = ['DownloadsService','$state'];
+  DownloadsListController.$inject = ['DownloadsService', '$state'];
 
   function DownloadsListController(DownloadsService, $state) {
     var vm = this;
 
     vm.downloads = DownloadsService.query();
-    vm.viewDetail = function(id){
+    vm.viewDetail = function (id) {
       $state.go('downloads.edit', {
         downloadId: id
       });
+    };
+    vm.subStr = function (text) {
+      var textRes = text.substr(0, 80);
+      textRes += '...';
+      return textRes;
     };
   }
 }());
